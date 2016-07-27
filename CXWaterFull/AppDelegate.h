9b66475187e861f44f7a9c67cb66de0c979d8d48
cangxue <752787086@qq.com>
2016-07-27 14:31:17 +0800

    Initial Commit

---
+++ CXWaterFull/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXWaterFull
+//
+//  Created by xiaoma on 16/7/27.
+//  Copyright © 2016年 xiaoma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CXWaterFull/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CXWaterFull
+//
+//  Created by xiaoma on 16/7/27.
+//  Copyright © 2016年 xiaoma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
